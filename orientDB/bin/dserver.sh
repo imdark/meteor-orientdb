637c9af1faa69c150c31c4524c45bfd5e6c54a2d
unknown <mkris@zahav.net.il>
2015-07-21 22:54:13 +0300

    remove orientdb

--- orientDB/bin/dserver.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-# Copyright (c) Orient Technologies LTD (http://www.orientechnologies.com)
-#
-${0/%dserver.sh/server.sh} -Ddistributed=true $*
--- orientDB/plugins/readme.txt
+++
@@ -1,4 +0,0 @@
-This is the folder to copy plugins. Plugins can be in the form of:
-- ZIP file
-- JAR file
-- directory
